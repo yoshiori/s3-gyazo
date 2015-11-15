27862ab394c84fd315fca752c3575fe9c775cd8a
Yoshiori SHOJI <yoshiori@gmail.com>
2015-11-15 13:59:03 -0600

    :bug: Fix template path bug

--- lib/s3/gyazo/client.rb
+++ lib/s3/gyazo/client.rb
@@ -2,7 +2,7 @@ require "aws-sdk-core"
 
 class S3::Gyazo::Client
   def self.run
-    Tempfile.create("") do |tmp|
+    ::Tempfile.create("") do |tmp|
       break unless system "screencapture -x -i \"#{tmp.path}\""
       filename = Time.now.strftime("%Y-%m-%d_%H-%M-%S.png")
       s3 = Aws::S3::Client.new(region: ENV["S3_GAZO_REGION"])
--- lib/s3/gyazo/version.rb
+++ lib/s3/gyazo/version.rb
@@ -1,5 +1,5 @@
 module S3
   module Gyazo
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
   end
 end
